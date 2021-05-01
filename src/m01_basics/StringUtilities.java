4e40f1dfdc09cbe7ba5f14e289a50e2868ae3f33
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-05-01 21:52:28 +0200

    m01_basics: smart step into

--- src/m01_basics/StringUtilities.java
+++ src/m01_basics/StringUtilities.java
@@ -5,7 +5,17 @@ public class StringUtilities {
     private int charsAdded = 0;
 
     public void addChar(StringBuilder sBuilder, char c){
-        this.sBuilder.append(c);       // tu jest błąd do debugowania
+        //this.sBuilder.append(c);       // tu jest błąd do debugowania
+        sBuilder.append(c);              // naprawiony błąd
         charsAdded++;
+    }
+
+    public String upperAndPrefix(String str){
+        String upper = str.toUpperCase();
+        return "Prefix " + upper;
+    }
+
+    public String addSuffix(String str){
+        return str + "__Suffix";
     }
 }
