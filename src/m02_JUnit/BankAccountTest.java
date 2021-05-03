d41491546559d9faccd5d1767b1463237d6c4b97
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-05-03 23:29:47 +0200

    m02_JUnit: testowanie kodu (2)

--- src/m02_JUnit/BankAccountTest.java
+++ src/m02_JUnit/BankAccountTest.java
@@ -1,12 +1,17 @@
 package m02_JUnit;
 
-import static junit.framework.TestCase.fail;
+import static junit.framework.TestCase.*;
 
 public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
-        fail("To be implemented");
+        //fail("To be implemented");
+        BankAccount account = new BankAccount("John", "Contre", 1000.00, BankAccount.CHECKING);
+        double firstDeposit = account.deposit(200.00, true);
+        assertEquals(1200.00, firstDeposit);
+        // poszczególne testy powinny być od siebie niezależne
+        // tzn. to, co dzieje się w jednym, nie powinno wpływać na inne
     }
 
     @org.junit.Test
@@ -15,7 +20,23 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() {
-        fail("To be implemented");
+    public void getBalance_deposit() {
+        BankAccount account = new BankAccount("John", "Contre", 1000.00, BankAccount.CHECKING);
+        account.deposit(340.00, true);
+        assertEquals(1340.00, account.getBalance());
+    }
+
+    @org.junit.Test
+    public void getBalance_withdraw(){
+        BankAccount account = new BankAccount("John", "Contre", 1000.00, BankAccount.CHECKING);
+        account.withdraw(150.00, true);
+        assertEquals(850.00, account.getBalance());
+    }
+
+    @org.junit.Test
+    public void isChecking_true(){
+        BankAccount account = new BankAccount("John", "Contre", 1000.00, BankAccount.SAVINGS);
+        assertTrue("The account is NOT a checking account", account.isChecking());
+        // message - wyświetla się, gdy test nie przejdzie
     }
 }
